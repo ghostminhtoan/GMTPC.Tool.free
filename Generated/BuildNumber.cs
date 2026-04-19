9278dc6259c9224f8970459e2e11fd95c97c7534
Ma Minh Toan <ghostminhtoan@gmail.com>
2026-04-19 11:51:51 +0700

    Allow Release and bin folders to be tracked in git
    - Uncommented [Rr]elease/ and [Bb]in/ in .gitignore to include built executables and dlls in repository
    - This allows the Release build exe to be pushed to git

--- Generated/BuildNumber.cs
+++ Generated/BuildNumber.cs
@@ -3,6 +3,6 @@ namespace GMTPC.Tool
 { 
     public static class BuildInfo 
     { 
-        public const string BUILD_NUMBER = "2026-04-19 / 11:07:19"; 
+        public const string BUILD_NUMBER = "2026-04-19 / 11:46:14"; 
     } 
 }
