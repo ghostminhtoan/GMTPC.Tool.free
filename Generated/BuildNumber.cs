b31b910984bb24d32cdbe1adc0cf409bba18de27
Ma Minh Toan <ghostminhtoan@gmail.com>
2026-04-23 12:28:31 +0700

    Increase system info panel text size

--- Generated/BuildNumber.cs
+++ Generated/BuildNumber.cs
@@ -3,6 +3,6 @@ namespace GMTPC.Tool
 { 
     public static class BuildInfo 
     { 
-        public const string BUILD_NUMBER = "2026-04-23 / 12:24:23"; 
+        public const string BUILD_NUMBER = "2026-04-23 / 12:28:05"; 
     } 
 }
