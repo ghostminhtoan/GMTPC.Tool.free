bd3aed92e2e0d4a62394414763eb8cc247b07f58
Ma Minh Toan <ghostminhtoan@gmail.com>
2026-05-15 09:55:28 +0700

    [UI] Fix defender header line break

--- Generated/BuildNumber.cs
+++ Generated/BuildNumber.cs
@@ -3,6 +3,6 @@ namespace GMTPC.Tool
 { 
     public static class BuildInfo 
     { 
-        public const string BUILD_NUMBER = "2026-05-15 / 09:08:44"; 
+        public const string BUILD_NUMBER = "2026-05-15 / 09:55:17"; 
     } 
 }
