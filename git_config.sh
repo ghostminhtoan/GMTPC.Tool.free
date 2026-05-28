7cc272b41fb0cf1ab537953251b2775ed3b6b210
GMTPC <ghostminhtoan@gmail.com>
2026-05-29 06:45:47 +0700

    Update repository links to GMTPC.Tool.free and add current safe.directory path

--- git_config.sh
+++ git_config.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 git config --global --add safe.directory "R:/HDD R/ZC SYMLINK/USERS/source/repos/ghostminhtoan/GMTPC.Tool"
+git config --global --add safe.directory "R:/HDD R/ZC SYMLINK/USERS/source/repos/ghostminhtoan/GMTPC.Tool - no copyright"
 git status
 git log -n 3 --oneline
 git push
--- git_push.bat
+++ git_push.bat
@@ -1,6 +1,7 @@
 @echo off
 set GIT_PATH="C:\Program Files\Git\bin\git.exe"
 %GIT_PATH% config --global --add safe.directory "R:/HDD R/ZC SYMLINK/USERS/source/repos/ghostminhtoan/GMTPC.Tool"
+%GIT_PATH% config --global --add safe.directory "R:/HDD R/ZC SYMLINK/USERS/source/repos/ghostminhtoan/GMTPC.Tool - no copyright"
 %GIT_PATH% status
 %GIT_PATH% log -n 3 --oneline
 %GIT_PATH% push
